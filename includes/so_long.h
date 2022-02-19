ec5ea48520a084889982f51eb172020f4a9ba961
coder <you@example.com>
2022-02-19 11:14:47 +0100

    added enum for x11 events

--- includes/so_long.h
+++ includes/so_long.h
@@ -29,6 +29,12 @@
 # define WALL_PATH "./img/wall.xpm"
 # define BLACK_PATH "./img/black.xpm"
 
+typedef enum e_x11events
+{
+	KeyPress = 2,
+	DestroyNotify = 17,
+}	t_x11events;
+
 typedef enum e_x11masks
 {
 	KeyPressMask = 1L << 0,
